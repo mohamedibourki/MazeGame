1dc26a5b409cb44bb80f10e5bb125de57337c597
nasasolicode <mohamedibourkioffice@gmail.com>
2023-10-20 16:58:40 +0100

    50%

--- src/DraggableElement.js
+++ src/DraggableElement.js
@@ -1,44 +1,43 @@
-    import React, { useEffect } from 'react';
-    import interact from 'interactjs'; // Make sure to install the 'interactjs' library
+import React, { useEffect } from 'react';
+import interact from 'interactjs';
 
-    function DraggableElements() {
-        useEffect(() => {
-            // target elements with the "draggable" class
-            interact('.draggable')
-                .draggable({
-                    inertia: true,
-                    modifiers: [
-                        interact.modifiers.restrictRect({
-                            restriction: 'parent',
-                            endOnly: true,
-                        }),
-                    ],
-                    autoScroll: true,
-                    listeners: {
-                        move: dragMoveListener,
-                    },
-                });
-        }, []);
+function DraggableElements() {
+    useEffect(() => {
+        interact('.draggable')
+            .draggable({
+                inertia: true,
+                modifiers: [
+                    interact.modifiers.restrictRect({
+                        restriction: 'parent',
+                        endOnly: true,
+                    }),
+                ],
+                autoScroll: true,
+                listeners: {
+                    move: dragMoveListener,
+                },
+            });
+    }, []);
 
-        function dragMoveListener(event) {
-            const target = event.target;
-            const x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
-            const y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
-            target.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
-            target.setAttribute('data-x', x);
-            target.setAttribute('data-y', y);
-        }
-
-        return (
-            <div className='dragos'>
-                <div id="drag-1" className="draggable">
-                    <p>Ball</p>
-                </div>
-                <div id="drag-2" className="draggable">
-                    <p>Goal</p>
-                </div>
-            </div>
-        );
+    function dragMoveListener(event) {
+        const target = event.target;
+        const x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
+        const y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+        target.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
+        target.setAttribute('data-x', x);
+        target.setAttribute('data-y', y);
     }
 
-    export default DraggableElements;
+    return (
+        <div className='dragos'>
+            <div id="drag-1" className="draggable"></div>
+            <div className='posi'></div>
+            <div className='posi1'></div>
+            <div className='posi2'></div>
+            <div className='posi3'></div>
+            <div className='posi4'></div>
+        </div>
+    );
+}
+
+export default DraggableElements;
--- src/Game.js
+++ src/Game.js
@@ -1,10 +1,15 @@
 import DraggableElement from "./DraggableElement";
+import Countdown from 'react-countdown';
 
 function Game() {
+    // Random component
+    const Completionist = () => <span>Time Over</span>;
     return (
         <>
             <header style={{ display: "flex", justifyContent: "space-around" }}>
-                <h1>Time : 10</h1>
+                <Countdown date={Date.now() + 5000}>
+                    <Completionist />
+                </Countdown>
                 <h1>Level : 1</h1>
             </header>
             <div className="border">
