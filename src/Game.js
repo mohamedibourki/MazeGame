aab502557326441dc7fd25cd102268d7d7219a6e
Mohamed Ibourki <mohamedibourkioffice@gmail.com>
2023-10-22 00:26:04 +0100

    adding things

--- src/Game.js
+++ src/Game.js
@@ -1,5 +1,5 @@
-import DraggableElement from "./DraggableElement";
 import Countdown from "react-countdown";
+import React from 'react';
 
 function Game() {
     const Completionist = () => <span>Time Over</span>;
@@ -10,22 +10,37 @@ function Game() {
         } else {
             return (
                 <span>
-                    {minutes}:{seconds}
+                    {`0${minutes}`}:{`0${seconds}`}
                 </span>
             );
         }
     };
+
+    function allowDrop(ev) {
+        ev.preventDefault()
+    }
+
+    function drag(ev) {
+        ev.dataTransfer.setData("text", ev.target.id)
+    }
+
+    function drop(ev) {
+        ev.preventDefault();
+        var data = ev.dataTransfer.getData("text");
+        ev.target.appendChild(document.getElementById(data));
+    }
     return (
         <>
             <header style={{ display: "flex", justifyContent: "space-around" }}>
                 <h1>
-                    <Countdown date={Date.now() + 10000} renderer={renderer} />
+                    <Countdown date={Date.now() + 9000} renderer={renderer} />
                 </h1>
 
                 <h1>Level : 1</h1>
             </header>
             <div className="border">
-                <DraggableElement />
+                <div className="ball" id="drag1" draggable="true" onDragStart={drag}></div>
+                <div className="goal" onDragOver={allowDrop} onDrop={drop}></div>
             </div>
         </>
     )
